221b2bd0565b4c44d41fa17e43ca112bb75ca113
Meryl Turner <merylturner08@gmail.com>
2017-06-06 14:05:02 -0700

    finished server.js TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,29 @@
 'use strict';
 
 // TODO: Initialize your project using NPM to create and populate a package.json file
+const express = require('express');
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
+const app = express();
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
-const bodyParser = require('body-parser').urlencoded({extended: true});
+const bodyParser = require('body-parser').urlencoded({ extended: true });
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-
+app.use(express.static('./public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/', function (request, response) {
+  response.sendFile('./public/new.html', { root: '.' });
+});
 
+app.get('*', function (request, response) {
+  console.log('hey');
+  response.status('404').sendFile('./public/404.html', {root: '.'});
+});
 
-app.post('/articles', bodyParser, function(request, response) {
+app.post('/articles', bodyParser, function (request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
@@ -22,6 +31,7 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('hello there');
 });
